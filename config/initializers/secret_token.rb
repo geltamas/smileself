5c7afac9782d0a75222f1cbbfffa6d26dc2ccdbd
Gellért Tamás <geltamas@gmail.com>
2014-03-08 21:15:07 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smileself::Application.config.secret_key_base = 'dcca141a522d00d31e3dbbf49f1941f2cd87b8823399ff02d09f6ad2d8cea93f7add5683409a871513df60370c06edf5efbb997c7414742d39e49ed16ea788b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smileself::Application.config.session_store :cookie_store, key: '_smileself_session'
